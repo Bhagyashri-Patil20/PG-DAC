c90ef0d26ab91fa9150354af1d6a0a62c649f7e6
Bhagyashri <bhagyashripatil453@gmail.com>
2024-06-14 12:47:01 +0530

    Project

---
+++ multiple.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2, mul; 
+    printf("Enter first number: "); 
+    scanf("%d", &num1); 
+    printf("Enter second number: "); 
+    scanf("%d", &num2); 
+    mul = num1 * num2; 
+    printf("Multiplication  %d", mul); 
+    return 0;
+}
---
+++ name.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("BHAGYASHRI SANJAY PATIL");
+    return 0;
+}
